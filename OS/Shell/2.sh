f201ae81b90ab6af0163494e788da1725b79307d
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2025-07-31 09:19:01 +0530

    initial

---
+++ OS/Shell/2.sh
@@ -0,0 +1,4 @@
+echo "Enter n:"
+read n
+
+echo $n
---
+++ OS/Shell/reverse.sh
@@ -0,0 +1,17 @@
+echo "Enter n:"
+read n
+
+rev=0
+sum=0
+original=$n
+
+while [ $n -gt 0 ]
+do
+    dig=$(( n % 10 ))
+    sum=$(( sum + dig ))
+    rev=$(( rev * 10 + dig ))
+    n=$(( n / 10 ))
+done
+
+echo "Reverse of $original is $rev"
+echo "Sum of digits is $sum"
